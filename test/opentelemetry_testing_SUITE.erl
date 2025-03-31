974345b5a612863af1e076cd432afba60f7dd5fd
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2025-03-31 19:05:24 +0200

    adding rebar3_efmt plugin

--- test/opentelemetry_testing_SUITE.erl
+++ test/opentelemetry_testing_SUITE.erl
@@ -9,19 +9,24 @@
 %% ct_suite callbacks
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
 all() ->
     [ensure_started_test].
+
 
 init_per_suite(Config) ->
     opentelemetry_testing:ensure_started(),
     Config.
 
+
 end_per_suite(Config) ->
     Config.
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% test cases
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 ensure_started_test(_Config) ->
     %% basic check if opentelemetry_testing:ensure_started/0
